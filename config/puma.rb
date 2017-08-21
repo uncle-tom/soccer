027de84bb9c547db536db792852b6e9fcfdd5fde
Vladimir Samojlov <v-moi@ya.ru>
2017-08-21 23:42:34 +0300

    edit puma

--- config/puma.rb
+++ config/puma.rb
@@ -54,3 +54,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+bind 'unix:///var/www/soccer/tmp/sockets/puma.sock'
